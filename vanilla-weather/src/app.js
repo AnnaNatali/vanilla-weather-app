82e721ee91e076d7e3799575a542df5075516beb
AnnaNatali <132666924+AnnaNatali@users.noreply.github.com>
2023-08-24 13:50:19 +0200

    showing the current temperature

---
+++ vanilla-weather/src/app.js
@@ -0,0 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElemet = document.querySelector("#city");
+  cityElemet.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "489fe1e0a45c1dabec6c6cd0f055d12a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
